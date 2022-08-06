c4729a250ae4e06454a3de7e434fb53dd31b0db4
李尚志 <shangzhi.li@aliyun.com>
2022-08-06 21:44:31 +0800

    完善OwnerAvailable接口定义

--- magnetic-sql-parser-statement/src/main/java/org/ibyte/sql/parser/sql/segment/generic/OwnerAvailable.java
+++ magnetic-sql-parser-statement/src/main/java/org/ibyte/sql/parser/sql/segment/generic/OwnerAvailable.java
@@ -2,9 +2,25 @@ package org.ibyte.sql.parser.sql.segment.generic;
 
 import org.ibyte.sql.parser.sql.SQLSegment;
 
+import java.util.Optional;
+
 /**
  * Owner available
  */
 public interface OwnerAvailable extends SQLSegment {
+
+    /**
+     * Get owner.
+     *
+     * @return owner
+     */
+    Optional<OwnerSegment> getOwner();
+
+    /**
+     * Set owner.
+     *
+     * @param owner owner
+     */
+    void setOwner(OwnerSegment owner);
 
 }
